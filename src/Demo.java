4011cbc5207562f48f94ff746ec447ba92331071
susmita218 <susmita218@gmail.com>
2021-09-05 14:34:56 +0530

    driver url update in branch

--- src/Demo.java
+++ src/Demo.java
@@ -9,7 +9,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Susmita\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Susmita\\chromedriver_win32_update\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
